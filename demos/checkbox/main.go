3b2392ce6b1585739b8ea9cb3279c69284a299dd
Gergely Bódi <vendelin8@gmail.com>
2023-01-29 14:02:31 +0100

    Replaced imports with my fork so I can use it while my PRs and issues are resolved

--- demos/checkbox/main.go
+++ demos/checkbox/main.go
@@ -1,7 +1,7 @@
 // Demo code for the Checkbox primitive.
 package main
 
-import "github.com/rivo/tview"
+import "github.com/vendelin8/tview"
 
 func main() {
 	app := tview.NewApplication()
--- demos/presentation/colors.go
+++ demos/presentation/colors.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 
 	"github.com/gdamore/tcell/v2"
-	"github.com/rivo/tview"
+	"github.com/vendelin8/tview"
 )
 
 const colorsText = `You can use style tags almost everywhere to partially change the color of a string. Simply put a color name or hex string in square brackets to change [::s]all[::-]the following characters' color. H[green]er[white]e i[yellow]s a[darkcyan]n ex[red]amp[white]le. [::i]The [black:red]tags [black:green]look [black:yellow]like [::u]this: [blue:yellow:u[] or [#00ff00[]. [:::https://github.com/rivo/tview]Hyperlinks[:::-] are also supported.`
